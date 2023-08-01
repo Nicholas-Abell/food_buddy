ca310705c75b689fc9342c316f2e3c3197bdeb8d
Nick <darkforceyoda@gmail.com>
2023-07-31 23:22:31 -0500

    backend created

--- server.js
+++ server.js
@@ -2,21 +2,23 @@
 // import mongoose from "mongoose";
 const express = require("express");
 const app = express();
+const cors = require("cors");
+// const mongoose = require("mongoose");
 
-const port = 5000
+app.use(cors());
+
+const port = 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.json({ x: "Hello World" });
 });
 
 app.listen(port, () => {
   console.log("server runnin on port: " + port);
+  // mongoose
+  //   .connect(process.env.MONGODB_CREDENTIALS)
+  //   .then(() => {
+  //     console.log("Mongoose Connected");
+  //   })
+  // .catch(console.error);
 });
-
-// mongoose
-//   .connect(process.env.MONGODB_CREDENTIALS)
-//   .then(() => {
-//     console.log("Mongoose Connected");
-//   })
-//   .catch(console.error);
-
