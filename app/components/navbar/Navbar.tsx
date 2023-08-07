d74add309ab443bf0edd59f93c3acd8021775896
Nick <darkforceyoda@gmail.com>
2023-08-07 11:28:35 -0500

    Navbar ui changes

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -6,18 +6,26 @@ type NavbarProps = {};
 const Navbar: React.FC<NavbarProps> = () => {
   return (
     <div className="absolute top-0 left-0 w-screen py-4">
-      <ul className="flex items-center justify-center gap-8">
+      <ul className="flex items-center justify-center gap-8 md:text-xl lg:text-2xl">
         <li>
-          <Link href="/">Home</Link>
+          <Link className="hover:text-gray-200" href="/">
+            Home
+          </Link>
         </li>
         <li>
-          <Link href="/saved-recipes">Your Recipes</Link>
+          <Link className="hover:text-gray-200" href="/saved-recipes">
+            Your Recipes
+          </Link>
         </li>
         <li>
-          <Link href="/create-recipe">Create Recipe</Link>
+          <Link className="hover:text-gray-200" href="/create-recipe">
+            Create Recipe
+          </Link>
         </li>
         <li>
-          <Link href="/auth">Your Profile</Link>
+          <Link className="hover:text-gray-200" href="/auth">
+            Your Profile
+          </Link>
         </li>
       </ul>
     </div>
