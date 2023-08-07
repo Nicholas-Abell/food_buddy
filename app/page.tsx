599a6b024a6dc454d80accf7fd3e4c2f0bbfe0bc
Nick <darkforceyoda@gmail.com>
2023-08-06 22:34:16 -0500

    .

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,8 @@ import React, { useEffect, useState } from "react";
 export default function Home() {
   const [message, setMessage] = useState("Loading");
 
-  useEffect(() => {
-    fetch("http://localhost:5000/")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setMessage(data.message);
-      });
-  }, []);
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 text-white">
-      <p className=" text-white text-lg">{message}</p>
     </main>
   );
 }
