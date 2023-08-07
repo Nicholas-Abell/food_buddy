2cbb52afd414a2ec044a77929501b85689d3e2d8
Nick <darkforceyoda@gmail.com>
2023-08-07 11:30:12 -0500

    imorted form comp for registration

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import Form from "./Form";
 
 type pageProps = {};
 
-const page: React.FC<pageProps> = () => {
-  return <div>Have a good coding</div>;
+const Auth: React.FC<pageProps> = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div>
+      <Form
+        username={username}
+        setUsername={setUsername}
+        password={password}
+        setPassword={setPassword}
+        label="Register"
+      />
+    </div>
+  );
 };
-export default page;
+export default Auth;
