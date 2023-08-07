de9fc8ee10b685056ad815cf9104a60897df4e9e
Nick <darkforceyoda@gmail.com>
2023-08-07 11:30:45 -0500

    Centered content

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="flex justify-center items-center w-screen h-screen">
         <Navbar />
         {children}
       </body>
