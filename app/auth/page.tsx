9edd7586be9ceb8dafde948be82ed2d96416955c
Nick <darkforceyoda@gmail.com>
2023-08-09 11:19:24 -0500

    added login form

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -5,21 +5,10 @@ import Form from "./Form";
 type pageProps = {};
 
 const Auth: React.FC<pageProps> = () => {
-
   return (
     <div className="flex justify-center items-center gap-8">
-      {/* <Form
-        username={username}
-        setUsername={setUsername}
-        password={password}
-        setPassword={setPassword}
-        label="Login"
-        isLoading={isLoading}
-        setIsLoading={setIsLoading}
-      /> */}
-      <Form
-        label="register"
-      />
+      <Form label="login" />
+      <Form label="register" />
     </div>
   );
 };
