4e3b2180f3c4de214f7aacbc05d9133f2d49e526
Nick <darkforceyoda@gmail.com>
2023-08-09 11:19:56 -0500

    feat: user logout

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,9 +1,21 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { useCookies } from "react-cookie";
+import { useRouter } from "next/navigation";
 
 type NavbarProps = {};
 
 const Navbar: React.FC<NavbarProps> = () => {
+  const [cookies, setCookies] = useCookies(["access_token"]);
+  const router = useRouter();
+
+  const logout = () => {
+    setCookies("access_token", "");
+    window.localStorage.removeItem("userID");
+    router.push("/auth");
+  };
+
   return (
     <div className="absolute top-0 left-0 w-screen py-4">
       <ul className="flex items-center justify-center gap-8 md:text-xl lg:text-2xl">
@@ -26,6 +38,9 @@ const Navbar: React.FC<NavbarProps> = () => {
           <Link className="hover:text-gray-200" href="/auth">
             Your Profile
           </Link>
+        </li>
+        <li>
+          <button onClick={logout}>LOGOUT</button>
         </li>
       </ul>
     </div>
