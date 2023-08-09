0aedea2271074d8014091cb52a8f3748546d4dd6
Nick <darkforceyoda@gmail.com>
2023-08-08 23:32:32 -0500

    Form registration

--- app/auth/Form.tsx
+++ app/auth/Form.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import axios from "axios";
 
 type FormProps = {
@@ -7,46 +8,67 @@ type FormProps = {
   password: string;
   setPassword: (e: string) => void;
   label: string;
+  isLoading: boolean;
+  setIsLoading: (arg: boolean) => void;
 };
 
-const Form: React.FC<FormProps> = ({
-  username,
-  setUsername,
-  password,
-  setPassword,
-  label,
-}) => {
-    
-  const onSubmit = () => {};
+const Form: React.FC<FormProps> = ({ label }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmit = async (e: any) => {
+    setIsLoading(true);
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/auth/register", {
+        username,
+        password,
+      });
+      alert("Registration Completed!");
+    } catch (error) {
+      console.error(error);
+    }
+    setIsLoading(false);
+  };
 
   return (
-    <div>
-      <form>
-        <h2>Register</h2>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => {
-              setUsername(e.target.value);
-            }}
-          ></input>
+    <div className="bg-gray-300">
+      <form className="flex flex-col justify-center items-center p-4 gap-4">
+        <h2 className="font-bold text-2xl">{label}</h2>
+        <div className="flex flex-col gap-4">
+          <div className="flex justify-end gap-2">
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            ></input>
+          </div>
+          <div className="flex justify-end gap-2">
+            <label htmlFor="password">Passoword:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            ></input>
+          </div>
         </div>
-        <div>
-          <label htmlFor="password">Passoword:</label>
-          <input
-            type="text"
-            id="password"
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          ></input>
-        </div>
-        <button type="submit">Submit</button>
+        <button
+          onClick={onSubmit}
+          className="bg-white p-2 rounded-lg hover:bg-gray-400"
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
+      <div>{isLoading && <p className="text-red-600">Loading</p>}</div>
     </div>
   );
 };
