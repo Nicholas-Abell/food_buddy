e426916b6256d6dd20ecec8c8b0dab1b58d2f38c
Nick <darkforceyoda@gmail.com>
2023-08-09 18:46:37 -0500

    Recipe route connected to backend  server

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ import cors from "cors";
 import mongoose from "mongoose";
 
 import { userRouter } from "./routes/users.js";
+import { recipesRouter } from "./routes/recipes.ts";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
 
 const port = 5000;
 
@@ -16,12 +18,10 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
-app.listen(port, () => {
+app.listen(5000, () => {
   console.log("server runnin on port: " + port);
   mongoose
-    .connect(
-
-    )
+    .connect("Hidden")
     .then(() => {
       console.log("Mongoose Connected");
     })
