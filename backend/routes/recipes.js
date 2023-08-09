73811396fd9cdcffb4f84ad5227985d3639327fa
Nick <darkforceyoda@gmail.com>
2023-08-09 18:43:48 -0500

    recipes route

---
+++ backend/routes/recipes.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import express from "express";
+import { RecipeModel } from "../models/Recipes.ts";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const response = await RecipeModel.find({});
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const recipe = new RecipeModel(req.body);
+  try {
+    const response = await recipe.save();
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+export { router as recipesRouter };
