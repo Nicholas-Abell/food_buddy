600b9f293f9ec7f6f54265b7923349ba6ff6dcca
Nick <darkforceyoda@gmail.com>
2023-08-12 16:22:01 -0500

    map through recipes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,30 @@
 "use client";
-import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
-  const [message, setMessage] = useState("Loading");
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/recipes");
+        setRecipes(response.data);
+      } catch (error) {}
+    };
+    fetchRecipes();
+  }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 text-white">
+      <h2>Recipes</h2>
+      <div className="flex flex-col">
+        <ul>
+          {recipes.map((recipe, key) => (
+            <li key={key}>{recipe}</li>
+          ))}
+        </ul>
+      </div>
     </main>
   );
 }
