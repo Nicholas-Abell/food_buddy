b90f41c2781fe9e802ddc7308409beb992a673dd
Nick <darkforceyoda@gmail.com>
2023-08-13 22:10:59 -0500

    recipe styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Image from "next/image";
 
 export default function Home() {
-  const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState<any[]>([]);
 
   useEffect(() => {
     const fetchRecipes = async () => {
@@ -18,12 +19,26 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 text-white">
       <h2>Recipes</h2>
-      <div className="flex flex-col">
-        <ul>
-          {recipes.map((recipe, key) => (
-            <li key={key}>{recipe}</li>
-          ))}
-        </ul>
+      <div className="flex flex-col justify-center items-center">
+        {recipes && (
+          <ul>
+            {recipes.map((recipe, key) => (
+              <li
+                className="flex flex-col justify-center items-center gap-4"
+                key={key}
+              >
+                <h2 className="font-bold uppercase text-2xl">{recipe.name}</h2>
+                <p>{recipe.instructions}</p>
+                <img
+                  src={recipe.imageUrl}
+                  alt={recipe.name + key}
+                  width={20}
+                  height={20}
+                />
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </main>
   );
