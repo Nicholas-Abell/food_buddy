0e2b398617e9fc011cc8f8582a9225e2b8a0ded9
Nick <darkforceyoda@gmail.com>
2023-08-14 22:58:33 -0500

    next image domains

--- next.config.cjs
+++ next.config.cjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.kasandbox.org",
+        port: "http://localhost:3000/",
+      },
+    ],
+  },
+};
 module.exports = nextConfig;
+
+//    domains: ["www.kasandbox.org"], // Add the domain(s) here
